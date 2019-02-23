5cc05afdbaeca884fbd9eb1d3c1362bed95bb286
Druidan <44009123+Druidan@users.noreply.github.com>
2019-02-23 14:10:40 -0600

    Did some adjustments, but currently the first incorrect image does not work.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,14 +43,14 @@ $(document).ready(function(){
                 1: "The Blue Whale",
                 2: "The Killer Whale",
                 3: "The Sperm Whale",
-                4: "The North Atlantic Right Whale"},
+                4: "The North Atlantic Right Whale",},
             unansweredText: "You didn't answer the question. Why? Are you too stunned by our titanic sizes?",
             correctText: "Correct. You have pleased Gilebrand, whome you see below. You know he is pleased because he has not crushed you beneath his fins yet.",
             incorrectText: "Ha! Incorrect, human! Look at Charles laughing at your pathetic intelligence down there!",
             correctImg: "swimming_whale.gif",
             incorrectImg: "mouthOpenWhale.jpg",
             correctImgClass: "cImg1",
-            incorrectImgClass: "icImg1"},
+            incorrectImgClass: "icImg1",},
         2: {
             question: "Let us continue, human. There are two types of whales. Which of these pairs are the correct names for these two types?",
             answer: "Baleen Whales and Toothed Whales",
@@ -58,7 +58,7 @@ $(document).ready(function(){
                 1: "Baleen Whales and Toothed Whales",
                 2: "Baleen Whales and Humped Whales",
                 3: "Arctic Whales and Tropical Whales",
-                4: "Breeching Whales and Toothed Whales"},
+                4: "Breeching Whales and Toothed Whales",},
             unansweredText: "Again, you remain silent. Are you taking this seriously, human?",
             correctText: "Correct, human! You have made Jeremy happy enough to anime sparkle! Would you look at that!",
             incorrectText: "Wrong, human! You make Jeremy down there laugh. He is a Toothed Whale, you know. Can you imagine what he wants to use those teeth for?",
@@ -73,14 +73,14 @@ $(document).ready(function(){
                 1: "Echolocation",
                 2: "The Doppler Effect",
                 3: "Sonar",
-                4: "Reverberative Hearing"},
+                4: "Reverberative Hearing",},
             unansweredText: "I see you remain silent out of fear for our superior senses!",
             correctText: "Correct! It seems that your meger intelligence has interested Meagan, our top-ranking beluga.",
             incorrectText: "Wrong, human! Meagan, the beluga, will echolocate all of your children, and she will feast!",
             correctImg: "interested_beluga.gif",
             incorrectImg: "whaleEatChild.gif",
             correctImgClass: "cImg3",
-            incorrectImgClass: "icImg3"},
+            incorrectImgClass: "icImg3",},
         4: {
             question: "Some of us have been planning for this war for long before we even evolved higher intelligence, human. What is the name of the whale with the horn weapon on its head?",
             answer: "The Narwhal",
@@ -88,14 +88,14 @@ $(document).ready(function(){
                 1: "The Narwhal",
                 2: "The Orca",
                 3: "The Minke",
-                4: "The Bowhead"},
+                4: "The Bowhead",},
             unansweredText: "You remain silent yet again. Perhaps you will make sounds when our Narwhal army comes.",
             correctText: "Correct, human! Our Narwhals have become quite skilled with their horns. Watch Melissa juggle the hoop.",
             incorrectText: "Incorrect, human! You will know their name when our armies of Narwhal come for you!",
             correctImg: "narwhal_dribbble.gif",
             incorrectImg: "narwhalArmy.gif",
             correctImgClass: "cImg4",
-            incorrectImgClass: "icImg4"},
+            incorrectImgClass: "icImg4",},
         5: {
             question: "You look dazed, human. Perhaps I will give you an easy question so that you do not faint before we can finish the game. When one of your foul whale murderers yells 'Thar she blows!,' what part of our anatomy is that murderer referencing?",
             answer: "The Blowhole",
@@ -103,14 +103,14 @@ $(document).ready(function(){
                 1: "The Blowhole",
                 2: "The Dorsal Fin",
                 3: "The Tail",
-                4: "The Head "},
+                4: "The Head ",},
             unansweredText: "Your continued silence bores me human.",
             correctText: "Correct! As a reward, you may now praise us.",
             incorrectText: "Wrong! Human, you have failed on such an easy question. We sneer at your paltry intelligence!",
             correctImg: "praiseWhale.gif",
             incorrectImg: "sneering_whale.gif",
             correctImgClass: "cImg5",
-            incorrectImgClass: "icImg5"},
+            incorrectImgClass: "icImg5",},
         6: {
             question: "What is the common name given to the two wings on a whale’s tail fin?",
             answer: "Flukes",
@@ -118,14 +118,14 @@ $(document).ready(function(){
                 1: "Flukes",
                 2: "Sails",
                 3: "Vestigial Flaps",
-                4: "Gliders"},
+                4: "Gliders",},
             unansweredText: "Perhaps your presence here is just as much a fluke as our tail fins are?",
             correctText: "Correct! Perhaps your presence here is not a fluke after all? As a reward, here is our prototype flying whale. Even your planes will be no match for us!",
             incorrectText: "Incorrect, human! Perhaps your presence here was a fluke after all?",
             correctImg: "flyingWhale.gif",
             incorrectImg: "nopeWhales.gif",
             correctImgClass: "cImg6",
-            incorrectImgClass: "icImg6"},
+            incorrectImgClass: "icImg6",},
         7: {
             question: "The wisest among you often come to watch us and bathe in our glory, but you often give our actions strange names. What is the behavior called when we slap the water with our tails?",
             answer: "Lobtailing",
@@ -133,14 +133,14 @@ $(document).ready(function(){
                 1: "Lobtailing",
                 2: "Dovetailing",
                 3: "Breaching",
-                4: "A Breach Slap"},
+                4: "A Breach Slap",},
             unansweredText: "My fellow whales, I believe they have sent us either a mute or an idiot. Perhaps both?",
             correctText: "Yes, human. Such a boring name. I would have prefered 'breach slap' myself. It is a much better name.",
             incorrectText: "Incorrect, human! And it should be called a breach slap! I breach slap you, human!",
             correctImg: "orca-yes.gif",
             incorrectImg: "breachslap.gif",
             correctImgClass: "cImg7",
-            incorrectImgClass: "icImg7"},
+            incorrectImgClass: "icImg7",},
         8: {
             question: "It is said that when one of your kind truly loves another that they would walk 1,000 miles to see them. THAT IS NOTHING. Our Gray Whales travel the farthest of all mammals on earth just because they like it! What is the longest distance they will travel when they migrate?",
             answer: "10,000 Miles",
@@ -148,14 +148,14 @@ $(document).ready(function(){
                 1: "3,000 Miles",
                 2: "8,000 Miles",
                 3: "10,000 Miles",
-                4: "6,000 Miles"},
+                4: "6,000 Miles",},
             unansweredText: "If you are too stunned to speak by such a great distance, then I imagine you will faint when you see that we have developed bionic legs! You are not safe on land, human!",
             correctText: "Correct, human! Are not whales the greatest species on earth? Watch Ferdinand dance below and tell me we are not the most beautiful of all creatures!",
             incorrectText: "Wrong, human! And if you think that distance is great, what will you think when you realize the land is no longer a barrier to us? Behold! We have developed bionic legs!",
             correctImg: "beautifulWhale.gif",
             incorrectImg: "leggedWhale2.gif",
             correctImgClass: "cImg8",
-            incorrectImgClass: "icImg8"},
+            incorrectImgClass: "icImg8",},
         9: {
             question: "As you can imagine, creatures of our impressive size can eat quite a bit! How much of the tiny shrimp like krill can an average blue whale consume during the summer feeding season?",
             answer: "Over 10,000 Pounds",
@@ -163,14 +163,14 @@ $(document).ready(function(){
                 1: "Over 14,000 Pounds",
                 2: "Over 7,000 Pounds",
                 3: "Over 2,000 Pounds",
-                4: "Over 10,000 Pounds"},
+                4: "Over 10,000 Pounds",},
             unansweredText: "The quiz is almost done, human, and if you continue to remain silent, so shall your kind be.",
             correctText: "Exactly! And we all agree that we love to eat!",
             incorrectText: "Wrong, human! I wonder how many humans that amount translates to. Barbara, let's test it on one of this one's party. Well, there they go.",
             correctImg: "Whales_Agree.gif",
             incorrectImg: "attackingWhale.gif",
             correctImgClass: "cImg9",
-            incorrectImgClass: "icImg9"},
+            incorrectImgClass: "icImg9",},
         10: {
             question: "You may believe your kind is past its barbaric history, and that you are no longer the same as those who hunted us in centuries past. Bah! Do you know how many whales were killed in the 20th Century?",
             answer: "Over 3 Million",
@@ -178,14 +178,14 @@ $(document).ready(function(){
                 1: "Over 1 Million",
                 2: "Over 100,000",
                 3: "Over 3 Million",
-                4: "Over 10,000"},
+                4: "Over 10,000",},
             unansweredText: "Your silence is deafening, human.",
             correctText: "Too true, human. This is what your kind has wrought. This is why our coming war is just. This is why you must perish. Let this marvelous sight be your last.",
             incorrectText: "No! No, human! So many more! My anger is boiling over!",
             correctImg: "breaching whale.gif",
             incorrectImg: "whalePacing.gif",
             correctImgClass: "cImg10",
-            incorrectImgClass: "icImg10"},
+            incorrectImgClass: "icImg10",},
     }
 
 //Constructors and Prototypes
@@ -264,18 +264,6 @@ const gameFunctions ={
         }
         //Declare that the question screen is up.
         questionScreenUp = true;
-        //if the answer buttons still have the correct or incorrect classes, remove them.
-        for (var i = 1; i <= 4; i++) {
-            let currentButton = ".aButton"+[i];
-            let targetButton = $(currentButton);
-            if (targetButton.hasClass("correctButton")) {
-                targetButton.removeClass("correctButton");
-            } else {
-                if (targetButton.hasClass("incorrectButton")) {
-                    targetButton.removeClass("incorrectButton");
-                }
-            }
-        };
         //Then display the current question text.
         $(".question-text").text(questions[currentQuestion].question);
         //Establish an array that will represent the numbers of each answer button in the HTML.
@@ -309,8 +297,6 @@ const gameFunctions ={
         $(".question-screen-row").addClass("buryIt");
         //Then it hides the answer buttons.
         $(".answers-row").addClass("buryIt");
-        //Then it reveals the answer image.
-        $(".answer-image").removeClass("buryIt");
         //Then, if the answer row elements are hidden it reveals the Answer Screen Elements.
         if ($(".answer-screen-row").hasClass("buryIt")){
             $(".answer-screen-row").removeClass("buryIt");
@@ -318,28 +304,30 @@ const gameFunctions ={
         if ($(".nextQ-timer-row").hasClass("buryIt")){
             $(".nextQ-timer-row").removeClass("buryIt");
         }
+        if ($(".answer-image").hasClass("buryIt")){
+            $(".answer-image").removeClass("buryIt");
+        }
         //display the timer
         $(".nextQTime").text(aTime);
         //Reveal the answer image area.
         $("answer-image").removeClass("buryIt");
         let cImage = questions[currentQuestion].correctImg
-        console.log(cImage);
         let icImage = questions[currentQuestion].incorrectImg
         console.log(icImage);
         //Then it determines if the answer was correct or incorrect by checking the currentAnswer state.
         if(isUnanswered === true) { //If it was unanswered...
-            $(".incorrectORcorrect-text").text(questions[currentQuestion].unansweredText);
-            $(".real-answer-text").text("The answer you DIDN'T GUESS is:" + questions[currentQuestion].answer);
-            $(".answer-image").append("<img class='currentImg " + questions[currentQuestion].incorrectImgClass + "' src='https://druidan.github.io/TriviaGame/assets/images/" + icImage + "'>");
+            $(".incorrectORcorrect-text").text(questions[currentQuestion].unansweredText); //Display text for an unanswered question.
+            $(".real-answer-text").text("The answer you DIDN'T GUESS is:" + questions[currentQuestion].answer); //Displays the correct answer.
+            $(".answer-image").append("<img class='currentImg " + questions[currentQuestion].incorrectImgClass + "' src='https://druidan.github.io/TriviaGame/assets/images/" + icImage + "'>"); //Add an image tag with image classes, and the image source
         } else{
             if (currentAnswer === true) { //If it's correct...
-                $(".incorrectORcorrect-text").text(questions[currentQuestion].correctText);
-                $(".real-answer-text").text("The answer was:" + questions[currentQuestion].answer);
-                $(".answer-image").append("<img class='currentImg " + questions[currentQuestion].correctImgClass + "' src='https://druidan.github.io/TriviaGame/assets/images/" + cImage + "'>");
-            } else{
-                $(".incorrectORcorrect-text").text(questions[currentQuestion].incorrectText);
-                $(".real-answer-text").text("The real answer is:" + questions[currentQuestion].answer);
-                $(".answer-image").append("<img class='currentImg " + questions[currentQuestion].incorrectImgClass + "' src='https://druidan.github.io/TriviaGame/assets/images/" + icImage + "'>");
+                $(".incorrectORcorrect-text").text(questions[currentQuestion].correctText); //Display text for a correct answer.
+                $(".real-answer-text").text("The answer was:" + questions[currentQuestion].answer); //Displays the correct answer.
+                $(".answer-image").append("<img class='currentImg " + questions[currentQuestion].correctImgClass + "' src='https://druidan.github.io/TriviaGame/assets/images/" + cImage + "'>"); //Add an image tag with image classes, and the image source
+            } else{ //If it's wrong
+                $(".incorrectORcorrect-text").text(questions[currentQuestion].incorrectText);  //Display text for an incorrect answer.
+                $(".real-answer-text").text("The real answer is:" + questions[currentQuestion].answer); //Displays the correct answer.
+                $(".answer-image").append("<img class='currentImg " + questions[currentQuestion].incorrectImgClass + "' src='https://druidan.github.io/TriviaGame/assets/images/" + icImage + "'>"); //Add an image tag with image classes, and the image source
             }
         }
         //Then it reveals the next question timer and calls the function that starts it counting down.
@@ -446,24 +434,7 @@ const gameFunctions ={
         answerScreenUp = false;
         isUnanswered = false;
     },
-    //The idea and form of this function, while I understand it and wish I thought of it before searching for it, came originally from an answer on StackOverflow, by CoolAJ86 at "https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array".
-    //This function is used to shuffle the allButtons array so that the buttons are populated with selections at random.
-    shuffle : function (allButtons) {
-        var currentIndex = allButtons.length, temporaryValue, randomIndex;
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            // And swap it with the current element.
-            temporaryValue = allButtons[currentIndex];
-            allButtons[currentIndex] = allButtons[randomIndex];
-            allButtons[randomIndex] = temporaryValue;
-        }
-        return allButtons;
-    }
 }
-
 
 //Global Event Captures
 
